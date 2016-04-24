8a84d5548338eba5226a9ad5dcfe50f66f3579d5
SlavaRa <ratdg@ya.ru>
2016-04-24 14:57:25 +0300

    Update Path.hx

--- system/io/Path.hx
+++ system/io/Path.hx
@@ -1,14 +1,11 @@
 package system.io;
 
-/**
- * @author SlavaRa
- */
 extern class Path {
 	/**
 	 * Combines two strings into a path.
-	 * @param	path1 The first path to combine.
-	 * @param	path2 The second path to combine.
-	 * @return 	The combined paths.
+	 * @param path1 The first path to combine.
+	 * @param path2 The second path to combine.
+	 * @return The combined paths.
 	 */
 	public static inline function combine2(path1:String, path2:String):String {
 		return haxe.io.Path.addTrailingSlash(path1) + path2;
@@ -16,10 +13,10 @@ extern class Path {
 	
 	/**
 	 * Combines three strings into a path.
-	 * @param	path1 The first path to combine.
-	 * @param	path2 The second path to combine.
-	 * @param	path3 The third path to combine.
-	 * @return  The combined paths.
+	 * @param path1 The first path to combine.
+	 * @param path2 The second path to combine.
+	 * @param path3 The third path to combine.
+	 * @return The combined paths.
 	 */
 	public static inline function combine3(path1:String, path2:String, path3:String):String {
 		return haxe.io.Path.addTrailingSlash(combine2(path1, path2)) + path3;
